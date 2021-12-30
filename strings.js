2df7e7c5b2e6a75fd0c4ae4fd006595ba392b36c
AdalbertCosta <contato@adalbertocosta.com>
2021-12-30 06:47:52 -0300

    conclusão instrings

---
+++ strings.js
@@ -0,0 +1,67 @@
+let = nome = "João"
+
+let sobrenome = "Pedro"
+
+typeof nome === "string"
+
+nome.concat(nome)
+
+let concatenado = nome.concat(sobrenome)
+
+typeof concatenado
+
+concatenado.leght
+
+let exemplo = new String("blablabla")
+
+typeof exemplo
+
+exemplo.length
+
+let vazio = ""
+
+typeof vazio
+
+nome
+
+nome[1]
+
+nome.length
+
+concatenado
+
+concatenado = nome + "" + sobrenome
+
+concatenado = nome + "\n" + sobrenome
+
+concatenado = nome + "\n\n" + sobrenome
+
+concatenado = `${nome} ${sobrenome}`
+
+concatenado = `${nome}
+
+${sobrenome}`
+
+concatenado = "\""
+
+let fraase = "Olá, tudo bem?"
+
+frase.split("")
+
+frase.split(" ")
+
+frase
+
+frase.includes("tudo") 
+
+frase.startswith("o")
+
+frase.startswith("R")
+
+frase.endsWith("?")
+
+frase.replace(",", "!")
+
+let stringModificada = frase.replace(",", "!")
+
+stringModificada
