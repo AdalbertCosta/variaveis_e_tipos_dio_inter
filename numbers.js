844374af9240f730fabbb34ccc394986d1d7ae06
AdalbertCosta <contato@adalbertocosta.com>
2021-12-31 06:46:58 -0300

    conclusão

--- numbers.js
+++ numbers.js
@@ -1,1 +1,33 @@
 let num = 100
+100 + 3
+
+100 / 5 
+
+num * 5
+
+num % 2
+
+typeof Math
+
+Math.PI
+
+let fiveByThree = 5 / 3
+
+fiveByThree
+
+Math.floor(fiveByThree)
+
+Math.ceil(fiveByThree)
+
+let porcent = "10%"
+
+percent 10
+
+percent = 10
+
+percent + "%"
+
+typeof percent
+
+percent toString()
+
