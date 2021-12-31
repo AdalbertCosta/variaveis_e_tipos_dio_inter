38c8628394a13f44cb12d6423cf7365e46c6c0b2
AdalbertCosta <contato@adalbertocosta.com>
2021-12-31 17:40:37 -0300

    conclusão

---
+++ arrays.js
@@ -0,0 +1,37 @@
+let array = []
+
+array.push(3)
+
+array
+
+array.push(2)
+
+array
+
+array.pop()
+
+array
+
+array.push(2)
+
+array.push(5)
+
+array.shift()
+
+array
+
+array.unshift(1)
+
+array
+
+for(let i = 0; 1 < array.length; i++) {
+console.log(array[i])
+}
+
+array.includes(3)
+
+array.every(item => item === 5)
+
+array.some(item => item === 5)
+
+array.filter.reverse()
