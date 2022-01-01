d02c20cbcee27aa1e3c916f0948f52d9cff73496
AdalbertCosta <contato@adalbertocosta.com>
2022-01-01 15:30:52 -0300

    conclusão

---
+++ objetos.js
@@ -0,0 +1,40 @@
+let obj = {}
+
+typeof obj
+
+obj.name = "julia"
+
+obj
+
+obj.age = 20
+
+obj
+
+Object.values(obj)
+
+Object.keys(obj)
+
+let person = {
+  name: "Julia",
+  age: 20,
+  adress: "Rua 2"
+}
+
+person
+
+person.name
+
+person["name"]
+
+person["numberOfSiblings"] =3 
+
+person
+
+let mom = "nameOfMom"
+
+person[mom] = "Maria"
+
+person
+
+
+
