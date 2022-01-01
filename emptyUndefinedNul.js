8c17b889ad257ba280a5fdcdf6f3c4c2849c54a7
AdalbertCosta <contato@adalbertocosta.com>
2022-01-01 15:48:59 -0300

    conclusão

---
+++ emptyUndefinedNul.js
@@ -0,0 +1,36 @@
+abacaxi
+
+typeof abacaxi
+
+let abacaxi
+
+let abacaxi = ""
+
+abacaxi
+
+typeof abacaxi
+
+abacaxi = null
+
+typeof abacaxi
+
+abacaxi
+
+abacaxi === true
+
+abacaxi === false
+
+let fruta = abacaxi
+
+let fruta = abacaxi === false
+
+fruta
+
+!abacaxi
+
+let maca
+
+!maca
+
+undefined === false
+
